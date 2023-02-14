404acc4e8f1764b4a3ad0955ea4b2fdf16681222
talidag <120104711+talidag@users.noreply.github.com>
2023-02-14 13:48:37 +0200

    Update data.js
    updated the images path

--- data.js
+++ data.js
@@ -1,50 +1,50 @@
 const dogsData = [
     {      
         name: "Eros",
-        avatar: "/images/dogs/eros.jpg",
+        avatar: "images/dogs/eros.jpg",
         age: 25,
         bio: "Dogs do speak, but only to those who know how to listen.",
     },
     {
         name: "Boy",
-        avatar: "/images/dogs/boy.jpg",
+        avatar: "images/dogs/boy.jpg",
         age: 4,
         bio: "What do dogs do on their day off? Can’t lie around – that’s their job.",
     },
     {
         name: "Queen",
-        avatar: "/images/dogs/queen.jpg",
+        avatar: "images/dogs/queen.jpg",
         age: 41,
         bio: "Dogs are not our whole life, but they make our lives whole.",
     },
     {
         name: "Mike",
-        avatar: "/images/dogs/mike.jpg",
+        avatar: "images/dogs/mike.jpg",
         age: 19,
         bio: "Some of my best leading men have been dogs and horses.",
     },
     {
         name: "Bobby",
-        avatar: "/images/dogs/bobby.jpg",
+        avatar: "images/dogs/bobby.jpg",
         age: 34,
         bio: "The better I get to know men, the more I find myself loving dogs.",
     },
     {
         name: "Jinx",
-        avatar: "/images/dogs/jinx.jpg",
+        avatar: "images/dogs/jinx.jpg",
         age: 33,
         bio: "I am fond of pigs. Dogs look up to us. Cats look down on us. Pigs treat us as equals.",
     },
     {
         name: "Prince",
-        avatar: "/images/dogs/prince.jpg",
+        avatar: "images/dogs/prince.jpg",
         age: 56,
         bio: "Scratch a dog and you’ll find a permanent job.",
     },
 
     {
         name: "Juli",
-        avatar: "/images/dogs/juli.jpg",
+        avatar: "images/dogs/juli.jpg",
         age: 29,
         bio: "Happiness is a warm puppy.",
     },
@@ -56,19 +56,19 @@ const fakeDogsData = [
 
     {
         name: "Fake dog",
-        avatar: "/images/dogs/fake-dog.jpg",
+        avatar: "images/dogs/fake-dog.jpg",
         age: 56,
         bio: "Nam libero tempore, cum soluta nobis est eligendi optio cumque.",
     },
     {
         name: "Another fake dog",
-        avatar: "/images/dogs/another-fake-dog.jpg",
+        avatar: "images/dogs/another-fake-dog.jpg",
         age: 29,
         bio: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
     },
     {
         name: "The fakest dog",
-        avatar: "/images/dogs/fakest-dog.jpg",
+        avatar: "images/dogs/fakest-dog.jpg",
         age: 56,
         bio: "Lorem ipsum dolor sit amet!",
     },
